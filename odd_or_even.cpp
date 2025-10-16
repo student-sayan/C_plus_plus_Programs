c9d46116012811997006d68f07ffeb613166438d
student-sayan <sayanroy702388@gmail.com>
2025-10-16 14:54:40 +0000

    chack the number is even or odd

---
+++ odd_or_even.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "Enter the first number = ";
+    cin >> a;
+
+    if (a % 2 == 0)
+    {
+        cout << "The number is Even \n";
+    }
+    else
+    {
+        cout << "The number is odd \n";
+    }
+}

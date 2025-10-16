aba67c3b43cf4700882ef9f78ff28a6a36d8f38d
student-sayan <sayanroy702388@gmail.com>
2025-10-16 14:03:10 +0000

    Area of circle

---
+++ AreaOfCircle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r, pai = 3.14159, ans;
+    cout << "Enter the radious(r) = ";
+    cin >> r;
+
+    ans = pai * (r * r);
+    cout << "Area of circle is = " << ans;
+}

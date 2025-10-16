7aa544db2cc928b6431c98c67ebd244db84fefd7
student-sayan <sayanroy702388@gmail.com>
2025-10-16 14:32:26 +0000

    Seuare of a number

---
+++ square_of_a_number_using_user_input.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float number,answer;
+    cout<<"Enter the number = ";
+    cin>>number;
+
+    answer = number * number;
+    cout<<"The square of a number is = "<<answer;
+}
